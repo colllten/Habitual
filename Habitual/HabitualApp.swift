dd2402e4761be6154faa47b1efb00094cf896ead
Colten Glover <coltenglover@yahoo.com>
2023-10-14 18:40:08 -0400

    Initial commit

---
+++ Habitual/HabitualApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitualApp.swift
+//  Habitual
+//
+//  Created by Colten Glover on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
