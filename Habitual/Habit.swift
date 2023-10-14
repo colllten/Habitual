6cc7fadf7a430d679528f2bf6e9a2dc12a090063
Colten Glover <coltenglover@yahoo.com>
2023-10-14 18:42:19 -0400

    Defined what a habit is

---
+++ Habitual/Habit.swift
@@ -0,0 +1,15 @@
+//
+//  Habit.swift
+//  Habitual
+//
+//  Created by Colten Glover on 10/14/23.
+//
+
+import Foundation
+
+struct Habit {
+    let name: String
+    let description: String
+    let consecDays: Int
+    let dateStarted: Date
+}
